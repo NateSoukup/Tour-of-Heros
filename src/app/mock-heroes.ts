7745b3eff78d7a46327d2ab294d15cb639758ee8
Nate Soukup <natesouk@gmail.com>
2020-11-23 19:04:07 -0600

    CSS updates and List View of Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice', power: "Real nice to people" },
+    { id: 12, name: 'Narco The Necro', power: "Control over post dead stuff" },
+    { id: 13, name: 'Bombasto', power: "It's in the name" },
+    { id: 14, name: 'Celeritas', power: "Controls stars using Zodiac Signs?" },
+    { id: 15, name: 'Magneta', power: "Is magenta" },
+    { id: 16, name: 'RubberMan', power: "Knock off version of Elastic Women" },
+    { id: 17, name: 'Dynama', power: "Dynomite TNT" },
+    { id: 18, name: 'Dr IQ', power: "Just an extra smart guy" },
+    { id: 19, name: 'Magma', power: "Like that pokemon magmar but slighlty more human-esk" },
+    { id: 20, name: 'Tornado', power: "The true last airbender" }
+];
